7d344786b5f85eaf8e324245a535190e9f5c9ff6
marktype <850623696@qq.com>
2016-05-16 17:11:41 +0800

    12

--- app/src/main/java/com/bruce/testgitdemo/MainActivity.java
+++ app/src/main/java/com/bruce/testgitdemo/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-
+//这是一个测试版的
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
