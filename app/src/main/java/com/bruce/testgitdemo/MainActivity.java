2857761b2edf895893769cdea22c2ce0a2b2f4a9
marktype <850623696@qq.com>
2016-05-16 17:35:39 +0800

    17

--- app/src/main/java/com/bruce/testgitdemo/MainActivity.java
+++ app/src/main/java/com/bruce/testgitdemo/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
      * 测试方法
      */
     public void tes() {
-        String name = "杨杰";
+        String name = "杨杰_方俊";
         //这是一个测试方法
         //这又是测试
         Log.d("tag","ahahhahahahhahah");
