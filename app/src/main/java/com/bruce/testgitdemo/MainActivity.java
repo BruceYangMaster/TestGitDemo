8397094729a8232a1958f8d82ab9c851d991e2f7
bruceyang <yang>
2016-05-16 17:18:41 +0800

    杨杰——测试

--- app/src/main/java/com/bruce/testgitdemo/MainActivity.java
+++ app/src/main/java/com/bruce/testgitdemo/MainActivity.java
@@ -4,10 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-//这是一个测试版的
+    //这是一个测试版的
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //测试一下
     }
 }
